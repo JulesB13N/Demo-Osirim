26d09d8868f3a4f321780296c589ac8f9b03faf5
JulesB13N <jules.bienassis@univ-tlse3.fr>
2024-01-24 12:07:43 +0100

    next

--- osirim.sh
+++ osirim.sh
@@ -5,4 +5,4 @@
 #SBATCH --mail-type=END             # Notification par email de la
 #SBATCH --mail-user=jules.bienassis@univ-tlse3.fr     # fin de l'exécution du job.
 
-srun singularity exec /logiciels/containerCollections/CUDA11/pytorch2-NGC-23-05-py3.sif python "$HOME/Demo-Osirim/python3 test.py
+srun singularity exec /logiciels/containerCollections/CUDA11/pytorch2-NGC-23-05-py3.sif $HOME/Demo-Osirim/venv/bin/python3 test.py
