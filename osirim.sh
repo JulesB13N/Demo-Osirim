4059c5b9ad21e4527d83b8373441929ea3f847fe
JulesB13N <jules.bienassis@univ-tlse3.fr>
2024-01-24 11:59:55 +0100

    correct batch syntax

--- osirim.sh
+++ osirim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Test_OSI   # Nom du Job  
 #SBATCH --cpus-per-task=1    
-#SBATCH --ouput=data.txt
+#SBATCH --output=data.txt
 #SBATCH --mail-type=END             # Notification par email de la
 #SBATCH --mail-user=jules.bienassis@univ-tlse3.fr     # fin de l'exécution du job.
 
